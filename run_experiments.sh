8dbeb860965da7eeb68bb0159dcfd742a00a0a3a
PierreExeter <pierre.aumjaud@gmail.com>
2020-03-30 18:46:44 +0200

    updated run_experiement

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,54 +3,20 @@
 
 for ((i=0;i<10;i+=1))
 do
-    python train.py \
-       --algo a2c \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log01.run
-
-    python train.py \
-       --algo acktr \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log02.run
-
-    python train.py \
-       --algo ppo2 \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log03.run
-
-    python train.py \
-       --algo ddpg \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log04.run
-
-    python train.py \
-       --algo sac \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log05.run
-
-    python train.py \
-       --algo td3 \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log06.run
-
-    python train.py \
-       --algo trpo \
-       --env ReacherBulletEnv-v0 \
-       --eval-episodes 10 \ 
-       --seed $i \
-       &> submission_log/log07.run
-
+    echo "1"
+    python train.py --algo a2c --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log01.run
+    echo "2"
+    python train.py --algo acktr --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log02.run 
+    echo "3"
+    python train.py --algo ppo2 --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log03.run
+    echo "4"
+    python train.py --algo ddpg --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log04.run
+    echo "5"
+    python train.py --algo sac --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log05.run
+    echo "6"
+    python train.py --algo td3 --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log06.run
+    echo "7"
+    python train.py --algo trpo --env ReacherBulletEnv-v0 --eval-episodes 10 --seed $i &> submission_log/log07.run
+    echo "6"
 done
 
